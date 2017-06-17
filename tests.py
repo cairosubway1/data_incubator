81c0af2606ee61436b133a6685db7015dd12ed7c
Andrew Hall <andrewhall4@gmail.com>
2017-06-16 22:21:16 -0400

    third test

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import unittest
 import io
-import traceback
 from app import app
 
 
@@ -10,12 +9,8 @@ class TestCase(unittest.TestCase):
 		self.client = app.test_client()
 
 	def test_upload_photo(self):			
-		result = self.client.post('/uploader', content_type='multipart/form-data', follow_redirects=True, data=dict(upload_var=(io.BytesIO(b'my file contents'), '2215767.jpg')))
-		lint = traceback.format_exc()
-		print(lint) 
-		print(result)
-		self.assertTrue(result.status_code == 200)
-	
+		result = self.client.post('/uploader', content_type='multipart/form-data', follow_redirects=True, data=dict(photo=(io.BytesIO(b'photo'), 'test.jpg')))
+		self.assertTrue(result.status_code == 200)	
 	
 if __name__ == "__main__":
     unittest.main()
