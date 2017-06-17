2166930f6bb09edd8163aed876b5bee7c52cc0d3
Andrew Hall <andrewhall4@gmail.com>
2017-06-16 22:56:18 -0400

    removed if

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config["TESTING"] = True
-
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-@app.route('/uploader', methods = ['GET','POST'] )
+@app.route('/uploader', methods = ['POST'] )
 def upload_photo():
-	if request.method == 'POST':
-		photo = request.files['photo']
-		resp = app.make_response(photo.read())	
-		resp.mimetype = 'image/jpeg'
-		return resp
+	photo = request.files['photo']
+	resp = app.make_response(photo.read())	
+	resp.mimetype = 'image/jpeg'
+	return resp
 	
 if __name__ == '__main__':
 	app.run(debug=True)
