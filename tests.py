1cd9a19d6586561f998a2af533d9468bb050e03e
Andrew Hall <andrewhall4@gmail.com>
2017-06-27 13:27:14 -0400

    changed test

--- tests.py
+++ tests.py
@@ -10,7 +10,10 @@ class TestCase(unittest.TestCase):
 
 	def test_upload_photo(self):			
 		result = self.client.post('/uploader', content_type='multipart/form-data', follow_redirects=True, data=dict(photo=(io.BytesIO(b'photo'), 'test.jpg')))
-		self.assertTrue(result.status_code == 200)	
+		content_headers = result.headers
+		content_type = content_headers.get('Content-Type')
+		self.assertTrue(content_type == 'image/jpeg')
+		#self.assertTrue(result.status_code == 200)
 	
 if __name__ == "__main__":
     unittest.main()
