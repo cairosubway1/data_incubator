36eabb2e3831f1001d08f6337e1e852e73087bb1
Andrew Hall <andrewhall4@gmail.com>
2017-06-28 13:22:03 -0400

    changed variable

--- tests.py
+++ tests.py
@@ -11,8 +11,8 @@ class TestCase(unittest.TestCase):
 	def test_upload_photo(self):			
 		result = self.client.post('/uploader', content_type='multipart/form-data', follow_redirects=True, data=dict(photo=(io.BytesIO(b'photo'), 'test.jpg')))
 		content_headers = result.headers		
-		content_type = content_headers.get('Content-Disposition')
-		self.assertTrue('test.jpg' in content_type)
+		content_disposition = content_headers.get('Content-Disposition')
+		self.assertTrue('test.jpg' in content_disposition)
 		#self.assertTrue(result.status_code == 200)
 	
 if __name__ == "__main__":
